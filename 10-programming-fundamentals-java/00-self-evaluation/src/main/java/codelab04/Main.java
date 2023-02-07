0aa334e3e02a02e0cfc4f28e181b1a4f14cb1977
Janos <janosdescheemaeker@hotmail.com>
2023-02-07 11:30:13 +0100

    codelab04 q1-3

--- 10-programming-fundamentals-java/00-self-evaluation/src/main/java/codelab04/Main.java
+++ 10-programming-fundamentals-java/00-self-evaluation/src/main/java/codelab04/Main.java
@@ -6,14 +6,36 @@ package codelab04;
 public class Main {
 
     public static String month(int i) {
-        throw new UnsupportedOperationException("implement me");
+        String months[] = new String[]{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        return months[i-1];
     }
 
     public static double calculatePrice(int amountOfElements) {
-        throw new UnsupportedOperationException("implement me");
+        double totalprice = 0;
+        for(int i = 0; i < amountOfElements; i++){
+            if(i < 10){
+                totalprice += 5;
+            }
+            else if (10 <= i && i < 15){
+                totalprice += 4;
+            }
+            else if (15 <= i){
+                totalprice += 3;
+            }
+        }
+        return totalprice;
     }
 
     public static String returnText(int i) {
-        throw new UnsupportedOperationException("implement me");
+        switch (i) {
+            case 1:
+                return "It's something";
+            case 2:
+                return "That's more like it";
+            case 3:
+                return "Way to go";
+            default:
+                return "That's a fail whale";
+        }
     }
 }
