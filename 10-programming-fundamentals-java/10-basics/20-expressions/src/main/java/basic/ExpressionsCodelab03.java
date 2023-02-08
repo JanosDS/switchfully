7ea21d176743f1affcad6b2a8487233e945661a1
Janos <janosdescheemaeker@hotmail.com>
2023-02-08 09:25:19 +0100

    Expressions codelab03

--- 10-programming-fundamentals-java/10-basics/20-expressions/src/main/java/basic/ExpressionsCodelab03.java
+++ 10-programming-fundamentals-java/10-basics/20-expressions/src/main/java/basic/ExpressionsCodelab03.java
@@ -12,6 +12,10 @@ public class ExpressionsCodelab03 {
         // Create a variable that holds the sum of 2 other variables (with values 4545.5 and 85454)
         // Print the result using: System.out.println("Printing sum: " + <YOUR_SUM_VARIABLE_NAME>);
         // Inspect the result, does it look as desired?
+        double numb1 = 4545.5;
+        double numb2 = 85454;
+        double sum = numb1 + numb2;
+        System.out.println("Printing sum: " + sum);
 
     }
 
