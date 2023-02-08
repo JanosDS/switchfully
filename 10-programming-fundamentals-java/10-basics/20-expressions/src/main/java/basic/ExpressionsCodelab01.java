6a820806e05d84c7e798eea467f12746cf2ad30e
Janos <janosdescheemaeker@hotmail.com>
2023-02-08 09:23:00 +0100

    Expressions codelab01

--- 10-programming-fundamentals-java/10-basics/20-expressions/src/main/java/basic/ExpressionsCodelab01.java
+++ 10-programming-fundamentals-java/10-basics/20-expressions/src/main/java/basic/ExpressionsCodelab01.java
@@ -12,11 +12,11 @@ public class ExpressionsCodelab01 {
         //Which of the following statements contain an expression?
         //Which part of the statement is an expression?
 
-        int someVariable;               //1
-        int someVariable2 = 2;          //2
-        int someVariable3 = 2 + 2;      //3
-        print(2 * 2);                   //4
-        printHelloWorld();              //5
+        int someVariable;               //1 not an expression
+        int someVariable2 = 2;          //2 not an expression
+        int someVariable3 = 2 + 2;      //3 Has expression 2+2
+        print(2 * 2);                   //4 has expression 2*2
+        printHelloWorld();              //5 not an expression
     }
 
     public static void print(int toPrint) {
