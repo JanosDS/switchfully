8b73f042077eb468c64712475392d34a804e6aa0
William Alegria Mira <william.alegria@outlook.com>
2023-02-10 11:59:21 +0100

    TRACK-UPDATE: Add extra edge testcase

--- 10-programming-fundamentals-java/10-basics/50-decision-making/src/test/java/basic/DecisionMakingCodelab04Test.java
+++ 10-programming-fundamentals-java/10-basics/50-decision-making/src/test/java/basic/DecisionMakingCodelab04Test.java
@@ -14,12 +14,13 @@ public class DecisionMakingCodelab04Test {
         assertThat(DecisionMakingCodelab04.playBlackjack(19, 22)).isEqualTo(19);
         assertThat(DecisionMakingCodelab04.playBlackjack(5, 4)).isEqualTo(5);
         assertThat(DecisionMakingCodelab04.playBlackjack(10, 10)).isEqualTo(10);
+        assertThat(DecisionMakingCodelab04.playBlackjack(22, 19)).isEqualTo(19);
     }
 
     @Test
     public void playBlackjack_givenBothCardValuesOver21_thenReturn0() throws Exception {
-        assertThat(DecisionMakingCodelab04.playBlackjack(22, 25)).isEqualTo(0);
-        assertThat(DecisionMakingCodelab04.playBlackjack(22, 22)).isEqualTo(0);
+        assertThat(DecisionMakingCodelab04.playBlackjack(22, 25)).isZero();
+        assertThat(DecisionMakingCodelab04.playBlackjack(22, 22)).isZero();
     }
 
 }
