e90fa07e5bebb561eb39e74ac4a29f53970033f2
Janos <janosdescheemaeker@hotmail.com>
2023-02-16 10:14:10 +0100

    Fix hitting ship on the same spot to sink

--- 10-programming-fundamentals-java/20-oop/10-objects-and-classes/21-classes/src/main/java/advanced/codelab03/Battleship.java
+++ 10-programming-fundamentals-java/20-oop/10-objects-and-classes/21-classes/src/main/java/advanced/codelab03/Battleship.java
@@ -1,15 +1,20 @@
 package advanced.codelab03;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Battleship {
     private String name;
-    private int[][] cells; //cell number, x,y
+
+    private ArrayList<int[]> cells = new ArrayList<>();
+    //private int[][] cells; //cell number, x,y
     private boolean isSunken;
 
     private int lives;
 
     public Battleship(String name, int[][] cells) {
         this.name = name;
-        this.cells = cells;
+        this.cells = new ArrayList<>(Arrays.asList(cells));
         this.isSunken = false;
         this.lives = cells.length;
     }
@@ -24,9 +29,9 @@ public class Battleship {
 
     public void setCoordinates(int[] startCoord, boolean horizontal) {
         int[] coord = startCoord;
-        for (int i = 0; i < cells.length; i++) {
-            cells[i][0] = coord[0];
-            cells[i][1] = coord[1];
+        for (int i = 0; i < cells.size(); i++) {
+            cells.get(i)[0] = coord[0];
+            cells.get(i)[1] = coord[1];
             if (!horizontal) {
                 coord[0] += 1;
             } else {
@@ -39,6 +44,7 @@ public class Battleship {
         for (int[] cell : cells) {
             if (coord[0] == cell[0] && coord[1] == cell[1]) {
                 lives--;
+                cells.remove(cell);
                 return true;
             }
         }
