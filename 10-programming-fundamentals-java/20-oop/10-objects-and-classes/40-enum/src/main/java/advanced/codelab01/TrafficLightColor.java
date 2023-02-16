934b82c57af88c9ec83abd6d3521260251dae29f
Janos <janosdescheemaeker@hotmail.com>
2023-02-16 11:05:44 +0100

    Codelabs enums advanced extra

--- 10-programming-fundamentals-java/20-oop/10-objects-and-classes/40-enum/src/main/java/advanced/codelab01/TrafficLightColor.java
+++ 10-programming-fundamentals-java/20-oop/10-objects-and-classes/40-enum/src/main/java/advanced/codelab01/TrafficLightColor.java
@@ -1,24 +1,34 @@
 package advanced.codelab01;
 
 public enum TrafficLightColor {
-    RED,
-    ORANGE,
-    GREEN;
+    RED("Red"),
+    RNG("Orange"),
+    GRN("Green");
+
+    private String label;
+
+    TrafficLightColor(String label) {
+        this.label = label;
+    }
 
     public boolean canChangeTo(TrafficLightColor color) {
-        if (this == GREEN && color == RED) {
+        if (this == GRN && color == RED) {
             return false;
         }
-        if (this == RED && color == ORANGE) {
+        if (this == RED && color == RNG) {
             return false;
         }
         return true;
     }
 
+    public String getLabel() {
+        return label;
+    }
+
     public String allColorsToString() {
         String str = "";
         for (TrafficLightColor color : TrafficLightColor.values()) {
-            str += color.toString() + ", ";
+            str += color.getLabel() + ", ";
         }
         return str;
     }
