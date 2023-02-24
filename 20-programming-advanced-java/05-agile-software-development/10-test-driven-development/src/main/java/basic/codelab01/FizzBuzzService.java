ffd7ee3b944c5613f80b0609070b9f2e4ece4f2c
Janos <janosdescheemaeker@hotmail.com>
2023-02-24 14:20:52 +0100

    TDD Codelab02

--- 20-programming-advanced-java/05-agile-software-development/10-test-driven-development/src/main/java/basic/codelab01/FizzBuzzService.java
+++ 20-programming-advanced-java/05-agile-software-development/10-test-driven-development/src/main/java/basic/codelab01/FizzBuzzService.java
@@ -1,4 +1,34 @@
 package basic.codelab01;
 
 public class FizzBuzzService {
+	private static int FIZZ_NUMBER = 3;
+	private static String FIZZ_STRING = "Fizz";
+	private static int BUZZ_NUMBER = 5;
+	private static String BUZZ_STRING = "Buzz";
+
+	public String[] createArrayFromOneTo(int i) {
+		if (i < 1) {
+			throw new IllegalArgumentException("Number needs to be bigger than one");
+		}
+		String[] array = new String[i];
+		for (int index = 0; index < array.length; index++) {
+			int number = index + 1;
+			array[index] = addFizzBuzz(number);
+		}
+		return array;
+	}
+
+	private String addFizzBuzz(int number) {
+		if (number % FIZZ_NUMBER == 0 && number % BUZZ_NUMBER == 0) {
+			return FIZZ_STRING + BUZZ_STRING;
+		}
+		if (number % FIZZ_NUMBER == 0) {
+			return FIZZ_STRING;
+		} else if (number % BUZZ_NUMBER == 0) {
+			return BUZZ_STRING;
+		} else {
+			return Integer.toString(number);
+		}
+	}
+
 }
--- 20-programming-advanced-java/05-agile-software-development/10-test-driven-development/src/test/java/basic/codelab01/FizzBuzzServiceTest.java
+++ 20-programming-advanced-java/05-agile-software-development/10-test-driven-development/src/test/java/basic/codelab01/FizzBuzzServiceTest.java
@@ -1,4 +1,43 @@
 package basic.codelab01;
 
-public class FizzBuzzServiceTest {
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+class FizzBuzzServiceTest {
+
+	@Nested
+	@DisplayName("Array maker tests")
+	class ArrayMaker {
+		private FizzBuzzService fizzBuzzService;
+
+		@BeforeEach
+		void setupFizzBuzzService() {
+			this.fizzBuzzService = new FizzBuzzService();
+		}
+
+		@Test
+		@DisplayName("Given a number x, return an array from one to x ")
+		void givenANumberX_returnArrayOfNumbersFromOneToX() {
+			String[] result = fizzBuzzService.createArrayFromOneTo(7);
+			String[] expectedResult = {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7"};
+			Assertions.assertArrayEquals(result, expectedResult);
+		}
+
+		@Test
+		@DisplayName("Given a number x smaller than one, return an illegal argument exception ")
+		void givenANumberXSmallerThanOne_returnIllegalArgumentException() {
+			IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+				fizzBuzzService.createArrayFromOneTo(0);
+			});
+		}
+
+		@Test
+		@DisplayName("Given a number 15, return an array from one to x ")
+		void givenANumber15_returnArrayOfNumbersFromOneTo15() {
+			String[] result = fizzBuzzService.createArrayFromOneTo(15);
+			String[] expectedResult = {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"};
+			Assertions.assertArrayEquals(result, expectedResult);
+		}
+	}
+
 }
