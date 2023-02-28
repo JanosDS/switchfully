daaf4ff728ef11353dc6493578c56bbfadf038dc
Janos <janosdescheemaeker@hotmail.com>
2023-02-28 15:15:02 +0100

    2d Arrays aditional CL1

--- 10-programming-fundamentals-java/10-basics/additional-20-arrays2D/src/main/java/advanced/codelab01/Zorro.java
+++ 10-programming-fundamentals-java/10-basics/additional-20-arrays2D/src/main/java/advanced/codelab01/Zorro.java
@@ -1,9 +1,19 @@
 package advanced.codelab01;
 
+import java.util.Arrays;
+
 public class Zorro {
 
-    public static char[][] generateZ(int size) {
-        throw new UnsupportedOperationException("implement me");
-    }
+	public static char[][] generateZ(int size) {
+		char[][] zorroChars = new char[size][size];
+
+		Arrays.fill(zorroChars[0], '@'); //fill firstline
+		Arrays.fill(zorroChars[size - 1], '@'); //fill last line
+		for (int i = 1; i < size - 1; i++) {
+			Arrays.fill(zorroChars[i], ' '); // place spaces
+			zorroChars[i][size - (i + 1)] = '@';
+		}
+		return zorroChars;
+	}
 
 }
