810ca05b08c984d0a2a7f3be58d87ab0e57c7ee0
Janos <janosdescheemaeker@hotmail.com>
2023-03-01 10:19:42 +0100

    Exceptions basic cl01

--- 20-programming-advanced-java/10-exceptions/10-exception-handling/src/main/java/basic/codelab01/PowerplantOperator.java
+++ 20-programming-advanced-java/10-exceptions/10-exception-handling/src/main/java/basic/codelab01/PowerplantOperator.java
@@ -2,8 +2,23 @@ package basic.codelab01;
 
 public class PowerplantOperator {
 
-    public static void main(String[] args) {
-        // Your code here
-    }
+	public static void main(String[] args) {
+		// Your code here
+		try {
+			NuclearPowerplant plant = new NuclearPowerplant();
+			System.out.println(plant.increasePower(77));
+		} catch (IllegalArgumentException e) {
+			System.err.println(e.getMessage());
+		}
+
+		try {
+
+			NuclearPowerplant plant2 = new NuclearPowerplant();
+			System.out.println(plant2.increasePower(11));
+		} catch (IllegalArgumentException e) {
+			System.err.println(e.getMessage());
+		}
+
+	}
 
 }
