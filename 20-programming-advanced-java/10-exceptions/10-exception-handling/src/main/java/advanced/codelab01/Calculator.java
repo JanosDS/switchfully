c91e526669d40e8ba519e01ef8634fc1047f1f8c
Janos <janosdescheemaeker@hotmail.com>
2023-03-01 10:32:32 +0100

    Exceptions advanced cl1

--- 20-programming-advanced-java/10-exceptions/10-exception-handling/src/main/java/advanced/codelab01/Calculator.java
+++ 20-programming-advanced-java/10-exceptions/10-exception-handling/src/main/java/advanced/codelab01/Calculator.java
@@ -1,32 +1,48 @@
 package advanced.codelab01;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
 
-    public static void main(String[] args) {
-        System.out.println("================================");
-        System.out.println("================================");
-        System.out.println("===========          ===========");
-        System.out.println("=========  CALCULATOR  =========");
-        System.out.println("===========          ===========");
-        System.out.println("================================");
-        System.out.println("=======                  =======");
-        System.out.println("=====   (divisions only)  ======");
-        System.out.println("=======                  =======");
-        System.out.println("================================");
-        System.out.println("================================");
+	public static void main(String[] args) {
+		System.out.println("================================");
+		System.out.println("================================");
+		System.out.println("===========          ===========");
+		System.out.println("=========  CALCULATOR  =========");
+		System.out.println("===========          ===========");
+		System.out.println("================================");
+		System.out.println("=======                  =======");
+		System.out.println("=====   (divisions only)  ======");
+		System.out.println("=======                  =======");
+		System.out.println("================================");
+		System.out.println("================================");
 
-        Scanner in = new Scanner(System.in);
-        System.out.print("Provide a numerator (int): ");
-        int numerator = in.nextInt();
+		Scanner in = new Scanner(System.in);
+		System.out.println("Provide a numerator (int): ");
+		int numerator = 10;
+		try {
+			numerator = in.nextInt();
+		} catch (InputMismatchException ex) {
+			System.err.println("(invalid numerator provided. Therefore it is set to value 10)");
 
-        System.out.print("Provide a denominator (int): ");
-        in.nextLine();
-        int denominator = in.nextInt();
+		}
 
-        var result = CalculatorService.divide(numerator, denominator);
-        System.out.println("The quotient of the division is: " + result);
-    }
+		System.out.println("Provide a denominator (int): ");
+		in.nextLine();
+		int denominator = 5;
+		try {
+			denominator = in.nextInt();
+		} catch (InputMismatchException ex) {
+			System.err.println("(invalid denominator provided. Therefore it is set to value 5)");
+		}
+
+		try {
+			var result = CalculatorService.divide(numerator, denominator);
+			System.out.println("The quotient of the division is: " + result);
+		} catch (ArithmeticException ex) {
+			System.err.println(ex.getMessage());
+		}
+	}
 
 }
