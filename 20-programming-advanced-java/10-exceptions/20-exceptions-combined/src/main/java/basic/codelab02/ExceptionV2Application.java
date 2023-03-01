0d7d30aa8dd629f5b078e585a737f0a90f8cf270
Janos <janosdescheemaeker@hotmail.com>
2023-03-01 14:05:01 +0100

    Exceptions combined cl2

--- 20-programming-advanced-java/10-exceptions/20-exceptions-combined/src/main/java/basic/codelab02/ExceptionV2Application.java
+++ 20-programming-advanced-java/10-exceptions/20-exceptions-combined/src/main/java/basic/codelab02/ExceptionV2Application.java
@@ -1,16 +1,22 @@
 package basic.codelab02;
 
+import java.io.IOException;
+
 /**
  * Read the README.md file
  */
 public class ExceptionV2Application {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        MyExceptionV2Service myExceptionService = new MyExceptionV2Service();
-        myExceptionService.doSomethingExceptional();
-        System.out.println("I can show you the world!");
+		MyExceptionV2Service myExceptionService = new MyExceptionV2Service();
+		try {
+			myExceptionService.doSomethingExceptional();
+		} catch (IOException exception) {
+			System.err.println(exception.getMessage());
+		}
+		System.out.println("I can show you the world!");
 
-    }
+	}
 
 }
--- 20-programming-advanced-java/10-exceptions/20-exceptions-combined/src/main/java/basic/codelab02/MyExceptionV2Service.java
+++ 20-programming-advanced-java/10-exceptions/20-exceptions-combined/src/main/java/basic/codelab02/MyExceptionV2Service.java
@@ -1,12 +1,15 @@
 package basic.codelab02;
 
+import java.io.IOException;
+
 /**
  * Read the README.md file
  */
 class MyExceptionV2Service {
 
-    void doSomethingExceptional() {
-        // throw an exception (read the readme.md file)
-    }
+	void doSomethingExceptional() throws IOException {
+		// throw an exception (read the readme.md file)
+		throw new IOException("Exception");
+	}
 
 }
