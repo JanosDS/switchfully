e0964ba29c2a3d8adb1538ee789006ddbfdeca80
Janos <janosdescheemaeker@hotmail.com>
2023-03-07 10:17:55 +0100

    Generics combined advanced codelab05 (Commented to avoid warnings)

--- 20-programming-advanced-java/30-generics/20-generics-combined/src/main/java/advanced/codelab05/SimpleList.java
+++ 20-programming-advanced-java/30-generics/20-generics-combined/src/main/java/advanced/codelab05/SimpleList.java
@@ -1,31 +1,31 @@
-package advanced.codelab05;
-
-import java.util.Arrays;
-import java.util.List;
-
-public class SimpleList<E> implements List<E> {
-
-
-	private E[] array;
-
-	@Override
-	public int size() {
-		return array.length;
-	}
-
-	@Override
-	public boolean add(E e) {
-		E[] tempArray = new E[array.length + 1];
-		for (int i = 0; i < array.length; i++) {
-			tempArray[i] = array[i];
-		}
-		tempArray[tempArray.length] = e;
-		array = tempArray;
-		return true;
-	}
-
-	@Override
-	public E get(int index) {
-		return array[index];
-	}
-}
+//package advanced.codelab05;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//public class SimpleList<E> implements List<E> {
+//
+//
+//	private E[] array;
+//
+//	@Override
+//	public int size() {
+//		return array.length;
+//	}
+//
+//	@Override
+//	public boolean add(E e) {
+//		E[] tempArray = new E[array.length + 1];
+//		for (int i = 0; i < array.length; i++) {
+//			tempArray[i] = array[i];
+//		}
+//		tempArray[tempArray.length] = e;
+//		array = tempArray;
+//		return true;
+//	}
+//
+//	@Override
+//	public E get(int index) {
+//		return array[index];
+//	}
+//}
