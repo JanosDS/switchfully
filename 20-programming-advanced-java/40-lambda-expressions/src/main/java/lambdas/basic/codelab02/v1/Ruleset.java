dde1708033390a5223249f0157b0a6cefdea0ab2
Janos <janosdescheemaeker@hotmail.com>
2023-03-07 12:10:25 +0100

    Lambdas basic codelab02 v1

---
+++ 20-programming-advanced-java/40-lambda-expressions/src/main/java/lambdas/basic/codelab02/v1/Ruleset.java
@@ -0,0 +1,7 @@
+package lambdas.basic.codelab02.v1;
+
+@FunctionalInterface
+interface Ruleset {
+	boolean isValid(int x);
+}
+
--- 20-programming-advanced-java/40-lambda-expressions/src/main/java/lambdas/basic/codelab02/v1/StrictCalculatorV1.java
+++ 20-programming-advanced-java/40-lambda-expressions/src/main/java/lambdas/basic/codelab02/v1/StrictCalculatorV1.java
@@ -5,18 +5,35 @@ package lambdas.basic.codelab02.v1;
  */
 public class StrictCalculatorV1 {
 
-    public static int multiplyBy2(int operator) {
-        return operator * 2;
-    }
+	public static int multiplyBy2(int operator, Ruleset rule) {
+		if (rule.isValid(operator)) {
+			return operator * 2;
+		}
+		return 0;
+	}
 
-    public static void main(String[] args) {
-        int result1 = StrictCalculatorV1.multiplyBy2(5);// should use ruleset #1
-        int result2 = StrictCalculatorV1.multiplyBy2(-2); // should use ruleset #2
-        int result3 = StrictCalculatorV1.multiplyBy2(8); // should use ruleset #3
+	public static void main(String[] args) {
+		int result1 = StrictCalculatorV1.multiplyBy2(5, new Ruleset() {
+			public boolean isValid(int x) {
+				return (x > 0 && x <= 5);
+			}
+		});// should use ruleset #1
+		int result2 = StrictCalculatorV1.multiplyBy2(-2, new Ruleset() {
 
-        System.out.println("Should be equal to 10: " + result1);
-        System.out.println("Should be equal to -4: " + result2);
-        System.out.println("Should be equal to 16: " + result3);
-    }
+			public boolean isValid(int x) {
+				return (x < 0 && -4 < x);
+			}
+		}); // should use ruleset #2
+		int result3 = StrictCalculatorV1.multiplyBy2(8, new Ruleset() {
+
+			public boolean isValid(int x) {
+				return (x > 7 && x < 14);
+			}
+		}); // should use ruleset #3
+
+		System.out.println("Should be equal to 10: " + result1);
+		System.out.println("Should be equal to -4: " + result2);
+		System.out.println("Should be equal to 16: " + result3);
+	}
 
 }
