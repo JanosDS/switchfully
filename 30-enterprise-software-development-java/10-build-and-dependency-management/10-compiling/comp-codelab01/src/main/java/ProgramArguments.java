20ca40c916f7aabbd878384c3d3f6ea52429501b
Janos <janosdescheemaeker@hotmail.com>
2023-03-13 09:55:58 +0100

    Compiling codelab01

--- 30-enterprise-software-development-java/10-build-and-dependency-management/10-compiling/comp-codelab01/src/main/java/ProgramArguments.java
+++ 30-enterprise-software-development-java/10-build-and-dependency-management/10-compiling/comp-codelab01/src/main/java/ProgramArguments.java
@@ -1,6 +1,16 @@
 public class ProgramArguments {
+	public static void main(String[] args) {
+		printArray(args);
+	}
 
-    static void printArray(String[] strings) {
-        System.out.println("This should print out the strings.");
-    }
+	static void printArray(String[] strings) {
+		System.out.println("Program arguments:");
+		if (strings != null && strings.length != 0) {
+			for (int i = 0; i < strings.length; i++) {
+				System.out.println(i + ":" + strings[i]);
+			}
+		} else {
+			System.out.println("None");
+		}
+	}
 }
