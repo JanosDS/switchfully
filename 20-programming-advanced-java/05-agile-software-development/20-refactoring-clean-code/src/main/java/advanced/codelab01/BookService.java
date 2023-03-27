a9639988318e871753a0ca2834ea6fdad42fc907
Janos <janosdescheemaeker@hotmail.com>
2023-03-27 14:37:06 +0200

    Refactoring ACL01

--- 20-programming-advanced-java/05-agile-software-development/20-refactoring-clean-code/src/main/java/advanced/codelab01/BookService.java
+++ 20-programming-advanced-java/05-agile-software-development/20-refactoring-clean-code/src/main/java/advanced/codelab01/BookService.java
@@ -11,29 +11,39 @@ import java.util.HashMap;
  */
 public class BookService {
 
-    public double shop(ArrayList<HarryPotterBook> b) {
-        double[] percentages = {0, 5, 10, 20, 25};
-        if(b.size() == 1) return 8;
-        HashMap<HarryPotterBookTitle, Integer> bMap = new HashMap<>();
-        // loop over books, make map of amount of books per type
-        for(HarryPotterBook b1 : b) {
-            bMap.put(b1.getTitleOfBook(),
-                    bMap.get(b1.getTitleOfBook()) == null ? 1 : bMap.get(b1.getTitleOfBook()) + 1);
-        }
-        // based on how big the set of books is,
-        // calculate the discount price!
-        double percentage = percentages[bMap.size() - 1];
-        double discountPrice = bMap.size() * 8 * ((100 - percentage) / 100);
-        double otherPrice = 0;
-        // caculate the total normal price without any discounts
-        for(int amount : bMap.values()) {
-            otherPrice += amount * 8;
-        }
-        // if there is a set of books (minimum 2 books), take the discountPrice
-        // in account, ignore otherwise
-        if(bMap.size() > 1) {
-            return otherPrice + discountPrice - (bMap.size() * 8); // pretty weird calculation, but hey it works correctly...
-        } return otherPrice;
-    }
+	private final double[] DISCOUNT_PERCENTAGES = {0, 5, 10, 20, 25};
+	private final double BASE_PRICE = 8;
+
+	public double shop(ArrayList<HarryPotterBook> bookList) {
+		if (bookList.size() == 1) {
+			return BASE_PRICE;
+		}
+		HashMap<HarryPotterBookTitle, Integer> bookMap = new HashMap<>();
+		// loop over books, make map of amount of books per type
+		for (HarryPotterBook book : bookList) {
+			bookMap.put(book.getTitleOfBook(),
+					bookMap.get(book.getTitleOfBook()) == null ? 1 : bookMap.get(book.getTitleOfBook()) + 1);
+		}
+		// based on how big the set of books is,
+		// calculate the discount price!
+		double percentage = getDiscountPercentage(bookMap.size());
+		double discountPrice = bookMap.size() * BASE_PRICE * ((100 - percentage) / 100);
+		double otherPrice = 0;
+		// caculate the total normal price without any discounts
+		for (int amount : bookMap.values()) {
+			otherPrice += amount * BASE_PRICE;
+		}
+		// if there is a set of books (minimum 2 books), take the discountPrice
+		// in account, ignore otherwise
+		if (bookMap.size() > 1) {
+			return otherPrice + discountPrice - (bookMap.size() * BASE_PRICE); // pretty weird calculation, but hey it works correctly...
+		}
+		return otherPrice;
+	}
+
+
+	private double getDiscountPercentage(int amountOfDifferentBooks) {
+		return DISCOUNT_PERCENTAGES[amountOfDifferentBooks - 1];
+	}
 
 }
