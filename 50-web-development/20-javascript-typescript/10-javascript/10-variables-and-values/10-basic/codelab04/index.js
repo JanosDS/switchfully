a61571d0df59c84c39c9c75015b5fbc7f8c390de
Janos <janosdescheemaeker@hotmail.com>
2023-04-24 11:33:33 +0200

    js basic codelabs

---
+++ 50-web-development/20-javascript-typescript/10-javascript/10-variables-and-values/10-basic/codelab04/index.js
@@ -0,0 +1,6 @@
+var vari;
+var foo = 5;
+console.log(typeof foo);
+foo = "bar";
+console.log(typeof foo);
+console.log(typeof vari);
---
+++ 50-web-development/20-javascript-typescript/10-javascript/10-variables-and-values/10-basic/codelab05/index.js
@@ -0,0 +1,4 @@
+let foo = 5;
+let bar = 3;
+foo = 10;
+bar = 11;
