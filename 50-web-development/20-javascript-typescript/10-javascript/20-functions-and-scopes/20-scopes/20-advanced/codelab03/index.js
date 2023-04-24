084159452ded133b492d67c5c08f6e08854076e5
Janos <janosdescheemaeker@hotmail.com>
2023-04-24 13:05:57 +0200

    js functions and scopes

--- 50-web-development/20-javascript-typescript/10-javascript/20-functions-and-scopes/20-scopes/20-advanced/codelab03/index.js
+++ 50-web-development/20-javascript-typescript/10-javascript/20-functions-and-scopes/20-scopes/20-advanced/codelab03/index.js
@@ -1,6 +1,26 @@
-function hoist() {
-  for (var i = 0; i < 3; i++) {
-    var h = 3; // becomes: 'h = 3'
-  }
-  console.log(h);
+function hoistVar() {
+    for (var i = 0; i < 3; i++) {
+        var h = 3; // becomes: 'h = 3'
+    }
+    console.log(h);
 }
+
+hoistVar();
+
+// function hoistLet() {
+//     for (let i = 0; i < 3; i++) {
+//         let h = 3; // becomes: 'h = 3'
+//     }
+//     console.log(h);
+// }
+//
+// hoistLet();
+
+function hoistConst() {
+    for (var i = 0; i < 3; i++) {
+        const h = 3; // becomes: 'h = 3'
+    }
+    console.log(h);
+}
+
+hoistConst();
